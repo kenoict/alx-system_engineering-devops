16aa2accf08a69e4b8993e7320882d77748dd3e4
Mafabi Kennedy <63707180+kenoict@users.noreply.github.com>
2022-06-28 16:05:49 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Installs puppet-lint version 1.1.0
-package { 'puppet-lint':
-  ensure   => '1.1.0',
-  provider => 'gem',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,4 +0,0 @@
-# Kills a process named killmenow
-exec { 'killmenow':
-  command => '/usr/bin/pkill -f killmenow'
-}
